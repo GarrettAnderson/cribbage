764396cce88f5c29c10c88fb85bad4de3c869271
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-03-05 21:45:33 -0500

    added the fetch call to get the cards data from the api

--- public/main.js
+++ public/main.js
@@ -1,2 +1,23 @@
 
 // fetch request to pull in the data for the card Deck
+
+let deckOfCards
+let deckOfCardsURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+
+// fetch(deckOfCardsURL)
+//   .then(res => res.json())
+//   .then(cards =>
+//
+//     console.log('here is the json', deckOfCards)
+//   ).catch(err => throw err);
+
+  fetch(deckOfCardsURL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      deckOfCards = data
+    })
+
+
+
+console.log(deckOfCards)
